9b7f360dbbc9e067c7478b005ba380a1a61c7dc0
cosimodamicis <damicis.cosimo@gmail.com>
2023-10-02 19:25:05 +0200

    Create engine_sensor.h

---
+++ engine_sensor.h
@@ -0,0 +1,8 @@
+#ifndef ENGINE_SENSOR_H
+#define ENGINE_SENSOR_H
+
+// Function prototypes for engine temperature sensor module
+void initializeEngineSensor();
+float readEngineTemperature();
+
+#endif
